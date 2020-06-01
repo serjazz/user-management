cd5e82cd23b55eff983f8949418595ab62d513b2
serjazz <info@cc-folio.ru>
2020-06-01 12:54:01 +0700

    form customisation

--- views/auth/passwordRecovery.php
+++ views/auth/passwordRecovery.php
@@ -14,9 +14,9 @@ $this->title = UserManagementModule::t('front', 'Password recovery');
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?=$this->title?></h3>
-    </div>
+    <!--<div class="panel-heading">
+        <h3 class="panel-title"><?/*=$this->title*/?></h3>
+    </div>-->
     <div class="panel-body">
 <div class="password-recovery">
 
--- views/auth/registration.php
+++ views/auth/registration.php
@@ -14,9 +14,9 @@ $this->title = UserManagementModule::t('front', 'Registration');
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?=$this->title?></h3>
-    </div>
+    <!--<div class="panel-heading">
+        <h3 class="panel-title"><?/*=$this->title*/?></h3>
+    </div>-->
     <div class="panel-body">
 <div class="user-registration">
 
