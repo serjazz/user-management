b2158e69bcdffec65a7fc07cb363fdd52a8715ff
serjazz <info@cc-folio.ru>
2020-06-05 11:28:53 +0700

    add profile

--- components/UserIdentity.php
+++ components/UserIdentity.php
@@ -22,168 +22,167 @@ use Yii;
  */
 abstract class UserIdentity extends ActiveRecord implements IdentityInterface
 {
-	/**
-	 * @inheritdoc
-	 */
-	public static function findIdentity($id)
-	{
-		return static::findOne($id);
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public static function findIdentityByAccessToken($token, $type = null)
-	{
-		return static::findOne(['auth_key' => $token, 'status' => User::STATUS_ACTIVE]);
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['auth_key' => $token, 'status' => User::STATUS_ACTIVE]);
+    }
 
-	/**
-	 * Finds user by username
-	 *
-	 * @param  string      $username
-	 * @return static|null
-	 */
-	public static function findByUsername($username)
-	{
-		return static::findOne(['username' => $username, 'status' => User::STATUS_ACTIVE]);
-	}
+    /**
+     * Finds user by username
+     *
+     * @param  string      $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username, 'status' => User::STATUS_ACTIVE]);
+    }
 
-	/**
-	 * Finds user by confirmation token
-	 *
-	 * @param  string      $token confirmation token
-	 * @return static|null|User
-	 */
-	public static function findByConfirmationToken($token)
-	{
-		$expire    = Yii::$app->getModule('user-management')->confirmationTokenExpire;
+    /**
+     * Finds user by confirmation token
+     *
+     * @param  string      $token confirmation token
+     * @return static|null|User
+     */
+    public static function findByConfirmationToken($token)
+    {
+        $expire    = Yii::$app->getModule('user-management')->confirmationTokenExpire;
 
-		$parts     = explode('_', $token);
-		$timestamp = (int)end($parts);
+        $parts     = explode('_', $token);
+        $timestamp = (int)end($parts);
 
-		if ( $timestamp + $expire < time() )
-		{
-			// token expired
-			return null;
-		}
+        if ( $timestamp + $expire < time() )
+        {
+            // token expired
+            return null;
+        }
 
-		return static::findOne([
-			'confirmation_token' => $token,
-			'status'             => User::STATUS_ACTIVE,
-		]);
-	}
+        return static::findOne([
+            'confirmation_token' => $token,
+            'status'             => User::STATUS_ACTIVE,
+        ]);
+    }
 
-	/**
-	 * Finds user by confirmation token
-	 *
-	 * @param  string      $token confirmation token
-	 * @return static|null|User
-	 */
-	public static function findInactiveByConfirmationToken($token)
-	{
-		$expire    = Yii::$app->getModule('user-management')->confirmationTokenExpire;
+    /**
+     * Finds user by confirmation token
+     *
+     * @param  string      $token confirmation token
+     * @return static|null|User
+     */
+    public static function findInactiveByConfirmationToken($token)
+    {
+        $expire    = Yii::$app->getModule('user-management')->confirmationTokenExpire;
 
-		$parts     = explode('_', $token);
-		$timestamp = (int)end($parts);
+        $parts     = explode('_', $token);
+        $timestamp = (int)end($parts);
 
-		if ( $timestamp + $expire < time() )
-		{
-			// token expired
-			return null;
-		}
+        if ( $timestamp + $expire < time() )
+        {
+            // token expired
+            return null;
+        }
 
-		return static::findOne([
-			'confirmation_token' => $token,
-			'status'             => User::STATUS_INACTIVE,
-		]);
-	}
+        return static::findOne([
+            'confirmation_token' => $token,
+            'status'             => User::STATUS_INACTIVE,
+        ]);
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getId()
-	{
-		return $this->getPrimaryKey();
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function getAuthKey()
-	{
-		return $this->auth_key;
-	}
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function validateAuthKey($authKey)
-	{
-		return $this->getAuthKey() === $authKey;
-	}
+    /**
+     * @inheritdoc
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
 
-	/**
-	 * Validates password
-	 *
-	 * @param  string  $password password to validate
-	 * @return boolean if password provided is valid for current user
-	 */
-	public function validatePassword($password)
-	{
-		return Yii::$app->security->validatePassword($password, $this->password_hash);
-	}
+    /**
+     * Validates password
+     *
+     * @param  string  $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
 
-	/**
-	 * Generates password hash from password and sets it to the model
-	 *
-	 * @param string $password
-	 */
-	public function setPassword($password)
-	{
-		if ( php_sapi_name() == 'cli' )
-		{
-			$security = new Security();
-			$this->password_hash = $security->generatePasswordHash($password);
-		}
-		else
-		{
-			$this->password_hash = Yii::$app->security->generatePasswordHash($password);
-		}
-	}
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        if ( php_sapi_name() == 'cli' )
+        {
+            $security = new Security();
+            $this->password_hash = $security->generatePasswordHash($password);
+        }
+        else
+        {
+            $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+        }
+    }
 
     /**
      * Generates "remember me" authentication key
      *
-     * @param int $length
-     * @throws \yii\base\Exception
+     * @param string $length
      */
     public function generateAuthKey($length=32)
-	{
-		if ( php_sapi_name() === 'cli' )
-		{
-			$security = new Security();
-			$this->auth_key = $security->generateRandomString($length);
-		}
-		else
-		{
-			$this->auth_key = Yii::$app->security->generateRandomString($length);
-		}
-	}
+    {
+        if ( php_sapi_name() == 'cli' )
+        {
+            $security = new Security();
+            $this->auth_key = $security->generateRandomString($length);
+        }
+        else
+        {
+            $this->auth_key = Yii::$app->security->generateRandomString($length);
+        }
+    }
 
-	/**
-	 * Generates new confirmation token
-	 */
-	public function generateConfirmationToken()
-	{
-		$this->confirmation_token = Yii::$app->security->generateRandomString() . '_' . time();
-	}
+    /**
+     * Generates new confirmation token
+     */
+    public function generateConfirmationToken()
+    {
+        $this->confirmation_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
 
-	/**
-	 * Removes confirmation token
-	 */
-	public function removeConfirmationToken()
-	{
-		$this->confirmation_token = null;
-	}
+    /**
+     * Removes confirmation token
+     */
+    public function removeConfirmationToken()
+    {
+        $this->confirmation_token = null;
+    }
 }
